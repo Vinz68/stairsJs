bb669fc7cd794ab07a1ae3dafe9f2089d4df87fd
Vincent van Beek <vin.van.beek@outlook.com>
2018-01-05 01:06:48 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+<img src="https://raw.githubusercontent.com/Vinz68/logTest/master/images/logTestImg.png" width="250"></img> 
+## stairsJs
+is a minimal NodeJS app for a rapberry pi to automatically drive the stairs LED lights (14 steps) using IR detection.
+It uses logging (bunyan) a database (mongoDB) a REST Webservice API (express) and of course GPIO (onoff)/
+
+## Purpose 
+The goal of this project is to build a nice led-light for my stairs (which I renovated recently) and to learn the raspberry PI, nodeJS and GPIO programming
+
+## Hardware
+TODO, make list of each component + link to spec.
+
+## Electric Scheme
+TODO, display all connection/wires
+
+## Pictures
+
+
+## Installation notes
+
+Prerequsites: You need node and npm installed on your system. You can check if you have it installed in a terminal window with:
+``` bash
+node -v
+npm -v
+```
+
+To install node + npm execute:
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+
+
+1. fork this package to your github account
+
+
+2. clone it from github to your server 
+``` bash
+git clone https://github.com/[your-account-name-here]/stairsJs.git
+```
+
+
+3. install its dependencies 
+```
+npm install
+```
+NOTE: Use npm install --only=production to install only dependencies, and not devDependencies,regardless of the value of the NODE_ENV environment variable.
+
+
+4. run the program
+
+use one of the following commands
+``` bash
+node stairsJs
+npm start
+gulp
+```
+or use PM2 (auto starts / auto restart the program after boot)
+``` bash
+pm2 start stairsJs.js
+```
+For the last option you need to install PM2. TODO: Provide link/instructions
+
+
+5. Execute the unit- and integration tests
+```
+gulp test
+```
+
+## Contribute
+
+Report a bug or a suggestion by posting an issue on the git repository (https://github.com/Vinz68/stairsJs/issues).
+
+
+
+
+ 
+## TODO List:
+ - [ ] include code quality check (JSLint or something else..)     
+ - [ ] add hardware overview
+ - [ ] add electric scheme
+ - [ ] add pictures
+ - [ ] add PM2 link/instructions ?
+ 
+
+
+
---
+++ stairsJs.js
@@ -0,0 +1,113 @@
+/* --------------------------------------------------------------------------------------------------
+   stairsJs - nodeJS program to automatically drive LED lights for a stair (14 steps), using IR detection.
+   - using bunyan as logging framework
+   - using onoff as GPIO package
+   2018-01-05 Vincent van Beek
+----------------------------------------------------------------------------------------------------- */
+"use strict";
+var express = require("express");           // Express web application framework. http://expressjs.com/
+var bodyParser = require("body-parser");    // Parse incoming request bodies in a middleware before your handlers,
+                                            // available under the req.body property. See https://github.com/expressjs/body-parser
+var Gpio = require('onoff').Gpio;           // Include onoff to interact with the GPIO
+var moment = require('moment');             // Moment is used to determine duration(s)
+
+var APPNAME = "stairsJs";                   // Name of this app used here and there
+var PORT = process.env.PORT || 8088;        // Node will listen on port from environment setting, or when not set to port number...
+
+var bunyan = require("bunyan");             // Bunyan is a simple and fast JSON logging library. https://github.com/trentm/node-bunyan
+
+
+/* -----------------------------------------------------
+    Start of main program
+------------------------------------------------------- */
+
+var log = bunyan.createLogger({             // Create a logger, to log application errors (and debug info)
+    name: APPNAME,                          // use the application name
+    src: true,                              // show source filename, function and line number
+                                            // Note: set to false in the production environment since it is time consuming
+    streams: [{
+        type: "rotating-file",              // Log files will "roll" over (automatic deletion after configured period)
+        path: "./logs/"+APPNAME+".log",     // Path and filename of the log file
+        period: "1d",                       // daily rotation
+        count: 30                           // keep 30 file copies (~1 month)
+    }],
+    serializers: {                          // usage of default serializers:
+        req: bunyan.stdSerializers.req,     // Bunyan's HTTP server request serializer with a suggested set of keys.
+        res: bunyan.stdSerializers.res      // Bunyan's HTTP server response serializer with a suggested set of keys.
+    }
+});
+
+log.info(APPNAME + " is starting... " );
+
+var startTime = moment(new Date());
+var endTime = moment(new Date());
+var LED = new Gpio(17, 'out'); 		        // Use GPIO.0 (pin 11) and specify that it is output
+var BTN = new Gpio(20, 'in', 'both');       // Use GPI0.28 (pin 38) and specify that it is input
+
+var blinkInterval = setInterval(blinkLED, 250); // Run the blinkLED function every 250ms
+
+function blinkLED() { 			        // Function to start blinking
+    if (LED.readSync() === 0) {		    // Check the pin state, if the state is 0 (or off)
+        log.info("Blink ON");
+        console.log("Blink ON");
+        LED.writeSync(1); 		        // Set pin state to 1 (turn LED on)
+    } else {
+        log.info("Blink OFF");
+        console.log("Blink OFF");
+        LED.writeSync(0); 		        // Set pin state to 0 (turn LED off)
+    }
+}
+
+function endBlink() { 			        // Function to stop blinking
+    clearInterval(blinkInterval); 	    // Stop blink intervals
+    LED.writeSync(0); 			        // Turn LED off
+
+    log.info("endBlink");
+    console.log("endBlink");
+}
+
+setTimeout(endBlink,5000); 		        // Stop blinking after 5 seconds
+
+setTimeout(endProgram, 72 * 60 *60000); // End program after 72 hours
+
+BTN.watch(function (err, value) {
+    if (err) {
+        console.log("BTN.watch error: " + err);
+        throw err;
+    }
+    endTime = moment(new Date());
+
+    log.info("BTN changed to value= " + value + " on: " + endTime.toString());
+    console.log("BTN changed to value= " + value + " on: " + endTime.toString());
+
+    var msecDiff = endTime.diff(startTime);
+    console.log("Duration in sec.=" + moment.duration(msecDiff).asSeconds() );
+    startTime = moment(new Date());
+
+    if (value==1)
+        console.log("Trigger 1");
+
+    LED.writeSync(value);
+});
+
+
+// Unexport GPIO to free resources
+function unExportGpio() {
+    console.log("unExportGpio");
+    LED.unexport();
+    BTN.unexport();
+}
+
+function endProgram() {
+    log.info(APPNAME+" has ended.");
+    console.log(APPNAME+" has ended.");
+    unExportGpio();
+}
+
+
+process.on('SIGINT', function () {
+    unExportGpio();
+    console.log(APPNAME+" has been terminated.");
+    process.exit(10);     // end program with code 10  (forced exit)
+});
+
