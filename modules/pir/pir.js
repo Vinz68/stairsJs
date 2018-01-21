03b8f0c0a925e75086e1bf5328e69d3c1200926b
Vincent van Beek <vin.van.beek@outlook.com>
2018-01-21 16:40:43 +0100

    sunTest added to determine daylight (when I do not want to use the stair lights)

--- modules/pir/pir.js
+++ modules/pir/pir.js
@@ -3,13 +3,13 @@
 // Infrared PIR module Motion Sensor Detector (SR501 HC-SR501)
 // Monitors one PIR, when motion has been detected an trigger event will be created and attached callback will be called.
 // 
-// 2018-01-07 Vincent van Beek
+// 2018-01-21 Vincent van Beek
 //-----------------------------------------------------------------------------------------------------
 "use strict";
 
 var Gpio = require('onoff').Gpio;           // Include onoff to interact with the GPIO
 
-exports.version = '0.0.5';
+exports.version = '0.1.0';
 
 class PIR {
 
@@ -19,7 +19,7 @@ class PIR {
         }
         // create (and store) the PIR digital input signal
         this.gpio = gpio;
-        this.sensorInput = new Gpio(this.gpio, 'in', 'both'); // input signal when PIR detects motion (rising)
+        this.sensorInput = new Gpio(this.gpio, 'in', 'both');             // input signal when PIR detects motion (rising)
         this.sensorInput.watch(this.trigger.bind(this));                  // when input changes 0=>1 our trigger function will be called
         this.log = options.log.child({ module: 'PIR', gpio: this.gpio }); // use bunyan log (from parent), add two keywords to identify which PIR/GPIO
         this.log.info('creating a PIR Handler for gpio:' + this.gpio);    // debug info (remove later)
@@ -32,11 +32,8 @@ class PIR {
             throw err;
         }
 
-        this.log.info("PIR signal changed to: " + value + " on: " + Date() );
-
         if (value==1) {
             this.log.info('PIR trigger detected on GPIO:' + this.gpio);
-            //console.log('PIR trigger detected on GPIO:' + this.gpio);                 
             this.callback(this.gpio);
         }
     }
--- sunTestJs.js
+++ sunTestJs.js
@@ -1,6 +1,18 @@
+/* --------------------------------------------------------------------------------------------------
+   sunTestJs - test program to determine when it is dark enough to allow light (test program for stairsJs)
+   - using suncalc to determine sunrise and sunset
+   2018-01-21 Vincent van Beek
+----------------------------------------------------------------------------------------------------- */
+"use strict";
+var SunCalc = require('suncalc');	    // SunCals is used to used sunrise / sunset time.
 
 
 
+/* -----------------------------------------------------
+    Start of main program
+------------------------------------------------------- */
+// using 'Immediately-Invoked Function Expression (IIFE)' pattern
+(function(){ 
         // get today's sunlight times for Amsterdam
         var times = SunCalc.getTimes(new Date(), 51.5, -0.1);
 
@@ -10,9 +22,9 @@
         // format sunsetStart time (bottom edge of the sun touches the horizon) from the Date object
         var sunsetStartStr = times.sunsetStart.getHours() + ':' + times.sunsetStart.getMinutes();
 
-       console.log("stairLight should work before: " + sunriseEndStr + " and after: " + sunsetStartStr );
+        console.log("stairLight should work before: " + sunriseEndStr + " and after: " + sunsetStartStr );
 
-       console.log("stairLight should work before: " + times.sunriseEnd + " and after " + times.sunsetStart );
+        console.log("stairLight should work before: " + times.sunriseEnd + " and after " + times.sunsetStart );
 
 
-
+})();
